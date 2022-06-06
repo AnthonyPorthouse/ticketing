5b6c469a1290b85e7c745664ea4b5c491db19eb8
Anthony Porthouse <anthony@porthou.se>
2022-06-06 16:49:44 +0100

    Initial ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,12 @@ import { json } from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send({
+        ping: 'pong'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Listening on 0.0.0.0:3000')
 })
